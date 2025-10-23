12db2c81e4ed6ab22f683810b645a473f248a9d9
tfeydev <tfeydeveloper@gmail.com>
2025-10-22 21:06:04 -0300

    update

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-// Declares the 'problems' folder as a module.
-// Rust will now look for 'src/problems.rs' OR 'src/problems/mod.rs'.
-pub mod problems;
--- src/problems/mod.rs
+++
@@ -1,6 +0,0 @@
-// This file acts as the index for the 'problems' module.
-// All individual LeetCode solutions are declared here.
-// Category: Easy
-pub mod two_sum;
-pub mod palindrome_number;
-pub mod roman_to_integer;
