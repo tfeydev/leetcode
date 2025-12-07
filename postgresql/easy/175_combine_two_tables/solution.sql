eaabb3a434e6c4d06de8eb22ff8bfc47d74b337d
techthordev <thorsten.fey@outlook.com>
2025-12-07 00:30:16 -0300

    chore: Remove java, postgresql directory from gh-pages branch as it is now consolidated on main

--- postgresql/easy/175_combine_two_tables/solution.sql
+++
@@ -1,9 +0,0 @@
--- SET search_path TO lc_175, public;
-
-SELECT 
-    p.firstname,
-    p.lastname,
-    a.city,
-    a.state
-FROM lc_175.person p
-LEFT JOIN lc_175.address a ON p.personid = a.personid;
